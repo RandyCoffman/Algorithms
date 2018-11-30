b820acb7e97bcb2093c93f36e53b720c81fd9423
unknown <randy.coffman@wvjc.edu>
2018-11-30 08:55:52 -0500

    first commit

---
+++ recursive_sum_array.py
@@ -0,0 +1,11 @@
+def sum_array(arr):
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        first_num = arr[0]
+        del arr[0]
+        sum = first_num + sum_array(arr)
+    return sum
+
+my_list = [1,2,3,4,5]
+print(sum_array(my_list))
